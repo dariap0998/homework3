50687172c3070a6ac19d6fca70431c255a5a92f9
darcyp <eelakoopfm@gmail.com>
2023-01-30 23:27:22 +0200

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+      public static void main(String[] args) {
+          task1();
+          task2();
+      }
+      public static void task1 () {
+          System.out.println("Задача 1");
+          int a = 5000000;
+          System.out.println("Значение переменной a с типом int равно " + a);
+          byte b = 1;
+          System.out.println("Значение переменной b с типом byte равно " + b);
+          short k = -22000;
+          System.out.println("Значение переменной k с типом short равно " + k);
+          long d = 1345673890;
+          System.out.println("Значение переменной d с типом long равно " + d);
+          float f = 3f;
+          System.out.println("Значение переменной f с типом float равно " + f);
+          double g = 3.21213;
+          System.out.println("Значение переменной g с типом double равно " + g);
+
+      }
+      public static void task2 () {
+          System.out.println("Задача 2");
+          //код для задачи 2
+      }
+}

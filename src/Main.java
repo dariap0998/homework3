aa2172bf029f6ca6df31e0454b15bb5f5cc70015
darcyp <eelakoopfm@gmail.com>
2023-01-30 23:43:03 +0200

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
       public static void main(String[] args) {
           task1();
           task2();
+          task3();
       }
       public static void task1 () {
           System.out.println("Задача 1");
@@ -11,9 +12,9 @@ public class Main {
           System.out.println("Значение переменной b с типом byte равно " + b);
           short k = -22000;
           System.out.println("Значение переменной k с типом short равно " + k);
-          long d = 1345673890;
+          long d = 13456738909L;
           System.out.println("Значение переменной d с типом long равно " + d);
-          float f = 3f;
+          float f = 3.03f;
           System.out.println("Значение переменной f с типом float равно " + f);
           double g = 3.21213;
           System.out.println("Значение переменной g с типом double равно " + g);
@@ -21,6 +22,18 @@ public class Main {
       }
       public static void task2 () {
           System.out.println("Задача 2");
-          //код для задачи 2
+          float apples = 27.12f;
+          long shrimps = 987678965549L;
+          double temp = 2.786;
+          short cats = 569;
+          short hours = -159;
+          short days = 27897;
+          byte years = 67;
+          System.out.println("Значения " + apples + ", " + shrimps + ", " + temp + ", " + cats + ", " + hours + ", " + days + " и " + years + " успешно инициализированы.");
+
+      }
+      public static void task3 () {
+          System.out.println("Задача 3");
+
       }
 }
